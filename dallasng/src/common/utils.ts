ac8838868efd0043686ba48386abc67529eb5985
dallas-ng <ngdallas1@gmail.com>
2021-07-17 00:59:55 +0800

    Update: function docs

--- dallasng/src/common/utils.ts
+++ dallasng/src/common/utils.ts
@@ -1,4 +1,29 @@
-export const sleep = async (ms: number) =>
-	await new Promise(resolve => setTimeout(resolve, ms));
+/**
+ * An async function that functions like python's time.sleep()
+ *
+ * @param ms - milliseconds to sleep for
+ * @returns void
+ */
+export const sleep = async (ms: number): Promise<void> => {
+	return await new Promise(resolve => setTimeout(resolve, ms));
+};
 
-export const mockApi = async () => await sleep(Math.random() * 1000);
+/**
+ * An async function that returns an empty promise after a random period within 2-3 second
+ *
+ * @returns void
+ */
+export const mockApi = async (): Promise<void> => {
+	return await sleep(getRandomNumber(0.1, 5) * 1000);
+};
+
+/**
+ * Generates a random number from a set of numbers
+ *
+ * @param min - minimum random number
+ * @param max - maximum random number
+ * @returns a random integer in between the min and max.
+ */
+export const getRandomNumber = (min: number, max: number): number => {
+	return Math.random() * (max - min) + min;
+};
--- dallasng/src/pages/api/mock.ts
+++ dallasng/src/pages/api/mock.ts
@@ -1,12 +1,13 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
-import { sleep } from '../../common/utils';
+import { getRandomNumber, sleep } from '../../common/utils';
 import { IEndpointProject } from '../../types/api.interface';
 
 const MockApi = async (
 	req: NextApiRequest,
 	res: NextApiResponse<IEndpointProject>
 ) => {
-	await sleep(Math.random() * 1000);
+	const timeToSleep = getRandomNumber(0.1, 1) * 1000;
+	await sleep(timeToSleep);
 
 	res.status(200).json({ projects: [], success: true });
 };
