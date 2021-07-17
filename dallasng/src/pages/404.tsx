6c462c6010425e81ae1b2ea7e8ef5b55b06595cd
dallas-ng <ngdallas1@gmail.com>
2021-07-17 21:18:00 +0800

    Update: Used layout for error pages

--- dallasng/src/pages/404.tsx
+++ dallasng/src/pages/404.tsx
@@ -1,23 +1,24 @@
 import { ArrowBackIcon } from '@chakra-ui/icons';
-import { Text, Box, Center } from '@chakra-ui/react';
+import { Text } from '@chakra-ui/react';
 import { Heading } from '@chakra-ui/react';
 import React from 'react';
-import { NextPage } from 'next';
 import Link from '../components/link';
+import DefaultLayout from '../layouts/default.layout';
+import { Page } from '../types/page';
 
-const IndexPage: NextPage = () => (
-	<Center minHeight="100vh">
-		<Box w={{ base: '90%', md: '80%', lg: '70%' }}>
-			<Heading as="h1" size="2xl" mb="5">
-				404 Page Not Found
-			</Heading>
+const Page404: Page = () => (
+	<>
+		<Heading as="h1" size="2xl" mb="5">
+			404 Page Not Found
+		</Heading>
 
-			<Text>
-				<ArrowBackIcon pb="1" boxSize="20px" />
-				<Link href="/">Go back to home page</Link>
-			</Text>
-		</Box>
-	</Center>
+		<Text>
+			<ArrowBackIcon pb="1" boxSize="20px" />
+			<Link href="/">Go back to home page</Link>
+		</Text>
+	</>
 );
 
-export default IndexPage;
+Page404.getLayout = page => <DefaultLayout>{page}</DefaultLayout>;
+
+export default Page404;
