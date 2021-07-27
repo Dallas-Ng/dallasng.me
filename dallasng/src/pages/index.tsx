7de18a53122d8072a12cddfa2e2f6fcf4d2fc2e5
dallas-ng <ngdallas1@gmail.com>
2021-07-27 19:16:44 +0800

    Update: Added links to front page

--- dallasng/src/pages/index.tsx
+++ dallasng/src/pages/index.tsx
@@ -3,7 +3,7 @@ import { ProjectCardList } from '@/src/components/project-card';
 import DefaultLayout from '@/src/layouts/default.layout';
 import { Page } from '@/src/types/page';
 import { ExternalLinkIcon } from '@chakra-ui/icons';
-import { Divider, Heading, Text } from '@chakra-ui/react';
+import { Divider, Heading, Stack, Text } from '@chakra-ui/react';
 import React from 'react';
 
 const IndexPage: Page = () => (
@@ -23,10 +23,25 @@ const IndexPage: Page = () => (
 
 		<Divider my="5" />
 
+		<Stack direction={['column', 'row']} spacing={['5px', '25px']}>
+			<Text>
+				<Link href="/about#Me">About Me</Link>
+			</Text>
+
+			<Text>
+				<Link href="/about#Resume">Resume</Link>
+			</Text>
+
+			<Text>
+				<Link href="/about#Skills">Skills</Link>
+			</Text>
+		</Stack>
+
+		<Divider my="5" />
+
 		<Heading as="h2" size="md" mb="5">
 			Projects
 		</Heading>
-
 		<ProjectCardList />
 	</>
 );
