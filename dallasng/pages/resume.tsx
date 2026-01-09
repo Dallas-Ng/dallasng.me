4c162d60414f3ccd26253803c4c4ef559462815e
dallas <ngdallas1@gmail.com>
2026-01-09 21:32:06 +0800

    fix

--- dallasng/pages/resume.tsx
+++ dallasng/pages/resume.tsx
@@ -1,145 +1,15 @@
-import {
-	Group,
-	Title,
-	Text,
-	createStyles,
-	Button,
-	Paper,
-	Divider,
-	Anchor
-} from '@mantine/core';
 import { NextPage } from 'next';
 import Layout from '../components/shared/layout';
-import { EXPERIENCE } from '../data/experience.data';
-import { IconFileDownload } from '@tabler/icons';
-import { SKILLS } from '../data/skills.data';
-import { EDUCATION } from '../data/education.data';
-import { NextLink } from '@mantine/next';
-import Head from 'next/head';
-import { toSanitisedLink } from '../utils/format';
+
 
 const ResumePage: NextPage = () => {
-	const { classes } = useStyles();
 
 	return (
 		<Layout headerText="Resume">
-			<Head>
-				<title>Resume - Dallas Ng</title>
-				<meta
-					name="description"
-					content="View my work experience, education and skills"
-				/>
-			</Head>
-
-			<Title>Resume</Title>
-
-			<Button
-				component="a"
-				download
-				href="/dallas_ng_resume.pdf"
-				leftIcon={<IconFileDownload size={16} />}
-				variant="outline"
-				mt="md"
-				mb="xl">
-				Download My Resume
-			</Button>
-
-			<Title order={2} mb="sm">
-				Education
-			</Title>
-
-			{EDUCATION.map((edu, i) => (
-				<Paper key={i} style={{ marginBottom: 30 }}>
-					<Group position="apart">
-						<div>
-							<Text weight="bold">{edu.specialisation}</Text>
-							<Text>{edu.institution}</Text>
-						</div>
-
-						<Text>
-							{edu.from} - {edu.to}
-						</Text>
-					</Group>
-
-					{edu.bullets.length > 0 && (
-						<ul style={{ marginTop: 0 }}>
-							<Text>
-								{edu.bullets.map((pointer, i) => (
-									<li key={i}>{pointer}</li>
-								))}
-							</Text>
-						</ul>
-					)}
-				</Paper>
-			))}
-
-			<Divider mb="md" mt={30} />
-
-			<Title order={2} mb="sm">
-				Experience
-			</Title>
-
-			{EXPERIENCE.map((exp, i) => (
-				<>
-					<span
-						className="anchor"
-						id={toSanitisedLink(`${exp.company}-${exp.title}`)}
-					/>
-
-					<div key={i} className={classes.expItem}>
-						<Title order={3}>{exp.title}</Title>
-
-						<Group position="apart">
-							{exp.website ? (
-								<Anchor href={exp.website} component={NextLink} target="_blank">
-									{exp.company}
-								</Anchor>
-							) : (
-								<Text>{exp.company}</Text>
-							)}
-
-							<Text>
-								{exp.from} - {exp.to}
-							</Text>
-						</Group>
-
-						<ul style={{ margin: '10px 0' }}>
-							{exp.description.map((desc, i) => (
-								<li key={i}>{desc}</li>
-							))}
-						</ul>
-
-						<Text>
-							<em>
-								{exp.technologies.length >= 1 && (
-									<>Technologies: {exp.technologies.join(', ')}</>
-								)}
-							</em>
-						</Text>
-					</div>
-				</>
-			))}
-
-			<Divider mb="md" />
-
-			<Title order={2} mb="sm">
-				Skills
-			</Title>
-
-			{SKILLS.map((skill, i) => (
-				<Paper mb="xs" key={i}>
-					<Text weight="bold">{skill.type}</Text>
-					<Text>{skill.items}</Text>
-				</Paper>
-			))}
+			<></>
 		</Layout>
 	);
 };
 
-const useStyles = createStyles(_theme => ({
-	expItem: {
-		marginBottom: 30
-	}
-}));
 
 export default ResumePage;
